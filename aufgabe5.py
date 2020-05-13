7cf01e63925b0bd4f5b1fd90301f5dccfe3415d1
Sowjetstuff <61296731+Sowjetstuff@users.noreply.github.com>
2020-05-13 19:59:03 +0200

    Delete aufgabe5.py

--- aufgabe5.py
+++
@@ -1,8 +0,0 @@
-print("Gib die Zahl n ein: ", end="")
-n = int(input())
-print()
-for Zahl in range(1,n+1):
-    if Zahl % 3 == 0: #or Bedingung
-        print(Zahl)
-    if Zahl % 5 == 0:
-        print(Zahl)
